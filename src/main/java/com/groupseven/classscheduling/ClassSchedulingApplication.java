28714b86f0f353d753544d9c063765f21708e32c
Kristel28 <108977077+Kristel28@users.noreply.github.com>
2023-12-02 07:41:56 +0800

    Add files via upload

--- src/main/java/com/groupseven/classscheduling/ClassSchedulingApplication.java
+++ src/main/java/com/groupseven/classscheduling/ClassSchedulingApplication.java
@@ -1,11 +1,18 @@
 package com.groupseven.classscheduling;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ClassSchedulingApplication {
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Manila"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClassSchedulingApplication.class, args);
 	}
